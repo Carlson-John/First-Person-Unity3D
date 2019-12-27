e8306ab03a1689522c25e8cb5fb1a387fedb7a57
John Carlson <39041290+Carlson-John@users.noreply.github.com>
2019-12-27 16:11:07 -0500

    Add files via upload
    Checked out more sources and fixed both PlayerMovement and MouseLook script.

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,30 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public CharacterController controller;
+    public Transform groundCheck;
+    public float speed =11f;
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float jumpHeight =3f;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Update()
+    {   
 
 
-    public float speed = 10.0F;
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y <0)
+        {
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
+        Vector3 move = transform.right * x + transform.forward * z;
 
+        controller.Move(move * speed *Time.deltaTime);
 
-    void Start ()
-    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
-        Cursor.lockstate = CursorLockMode.Locked;
-    }
-
-    void Update ()
-    {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float straffe = Input.GetAxis("Horizontal") * speed;
-        translation *= Time.deltaTime;
-        straffe *= Time.deltaTime;
-
-        transform.Translate(straffe, 0, translation);
-        if (Input.GetKeyDown("escape"))
-            Cursor.lockstate = CursorLockMode.None;
-
-
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
     }
 }
